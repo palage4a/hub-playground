5badce991c4bb9f8162206233ed80d0873e4d949
palage4a <vvnvvplg@gmail.com>
2025-05-10 10:52:13 +0300

    rust: minigrep: send errors into stderr instead of stdout

--- rust-playground/minigrep/src/main.rs
+++ rust-playground/minigrep/src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err|{
-        println!("Arguments parsing error: {err}");
+        eprintln!("Arguments parsing error: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Aplication error: {e}");
+        eprintln!("Aplication error: {e}");
         process::exit(1);
     }
 }
